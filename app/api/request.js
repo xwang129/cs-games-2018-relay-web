41a0cd9d568ae41954e846f723d8c99e10f8feee
Brandon Willett <bcwillett1@gmail.com>
2018-03-24 14:44:26 -0400

    the very worst csgames event I've ever done

--- app/api/request.js
+++ app/api/request.js
@@ -1,7 +1,32 @@
 import { fakeFetch } from './fakeFetch';
 
+// Hi this is brandon
+// This function gets called when you hit the button on the screen
+
+// Node.js and webpack and react.js horrify me and I am not good at this
+// Why is this call "mocked" it looks ok to me?
+
+// What is any of this
+// I am very sorry but I cannot make any sense of this "magic"
+// I can't learn the whole JS ecosystem in an hour!
+
+// Furthermore after much time looking I cannot find where
+// this function is "attached" to the button like in any normal world
+
+// I'd like to make a new button that does some new things, like display orders
+// But, there's no HTML anywhere, I don't even know where I could make one :\
+
+// Okay wait, last 5 minutes I've found it
+// It's all inside the Wallet folder inside of containers
+// Inside index.js inside Wallet
+// maybe look there?
+
 const request = (urlGetter, options) => {
   console.log('this is a mocked call');
+
+  var orders = require('./orders.json');
+  console.log(orders); // wow look it shows up in the dev console
+
   return fakeFetch(urlGetter, options);
 };
 export default request;
