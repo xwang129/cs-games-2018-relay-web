364fad4e5324cb62347b91e4fd0a2bb4d2e761f1
xwang129 <xwang129@u.rochester.edu>
2018-03-24 16:03:22 -0400

    Update constants.js

--- app/containers/Wallet/constants.js
+++ app/containers/Wallet/constants.js
@@ -1,2 +1,7 @@
 export const LOAD_WALLETS = 'crypto_exchange/App/LOAD_WALLETS';
+//I am so sorry I did not write anything
+//I think that similar to LOAD_WALLETS, we need more actions to be added here.
+//maybe like this?
+//export const LOAD_WALLETS = 'crypto_exchange/App/CREATE_WALLET';
+
 export const WALLETS = 'wallets';
