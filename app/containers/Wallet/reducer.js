2bffc475cc856ff2ed9cc0eafaa1b0cc2713df88
xwang129 <xwang129@u.rochester.edu>
2018-03-24 15:59:10 -0400

    Update reducer.js

--- app/containers/Wallet/reducer.js
+++ app/containers/Wallet/reducer.js
@@ -19,6 +19,10 @@ function walletReducer(state = WALLETS_REDUCER_INITIAL_STATE, action) {
     case `${LOAD_WALLETS}${SUCCESS}`:
       return state
       .set('wallets', fromJS(action.entity));
+    //Cause there exists a switch statement, I think we need to add more cases(actions)
+    //and the actions may be the same with what we add to the Wallet/actions.js file
+      
+      
     default:
       return state;
   }
