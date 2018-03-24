35d422c5300b66c899d5a3466f851885d76df31d
xwang129 <xwang129@u.rochester.edu>
2018-03-24 15:56:06 -0400

    Update actions.js

--- app/containers/Wallet/actions.js
+++ app/containers/Wallet/actions.js
@@ -1,5 +1,6 @@
 import { LOAD_WALLETS } from './constants';
-
+//I think that there are much more actions available to add here, but I really cannot find them
+//Maybe we should develop actions by ourselves? like create new one? or show the status of an account?
 export function loadWallets() {
   return {
     type: LOAD_WALLETS,
