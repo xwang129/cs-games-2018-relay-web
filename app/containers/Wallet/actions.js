89a1c82de9de1504190085b3abf8d9541a1a6074
xwang129 <xwang129@u.rochester.edu>
2018-03-24 16:12:06 -0400

    Update actions.js

--- app/containers/Wallet/actions.js
+++ app/containers/Wallet/actions.js
@@ -1,5 +1,6 @@
 import { LOAD_WALLETS } from './constants';
-
+//I think that there are much more actions available to add here, but I really cannot find them
+//I know that they are from constants
 export function loadWallets() {
   return {
     type: LOAD_WALLETS,
